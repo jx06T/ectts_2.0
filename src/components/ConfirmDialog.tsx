4975e7c1127720b3277ecdee057cbc799980a093
jx06T <50313tjx06@gmail.com>
2024-08-28 18:13:47 +0800

    手機確認框換行

--- src/components/ConfirmDialog.tsx
+++ src/components/ConfirmDialog.tsx
@@ -22,9 +22,9 @@ function createConfirmDialog(message: string, onConfirm: Function, onCancel: Fun
         };
 
         return (
-            <div className="z-40 confirm-dialog-overlay flex justify-center fixed w-full top-20 left-0 right-0  px-2">
-                <div className=" border-l-4 border-l-purple-700 confirm-dialog w-96 p-3 pb-2 bg-purple-200 rounded-md shadow-md">
-                    <pre className=' text-wrap'>{message}</pre>
+            <div className="z-40 confirm-dialog-overlay flex justify-center fixed w-full top-20 left-1 right-1 px-4 pl-2">
+                <div className=" border-l-4 border-l-purple-700 confirm-dialog max-w-96 p-3 pb-2 bg-purple-200 rounded-md shadow-md">
+                    <pre className=' text-wrap whitespace-pre-wrap'>{message}</pre>
                     <div className=' w-full flex justify-end space-x-4 mt-4'>
                         <button className=' cursor-pointer px-2 rounded-md bg-red-400 hover:bg-red-500' onClick={handleConfirm}>Confirm</button>
                         <button className=' cursor-pointer px-2 rounded-md bg-purple-400 hover:bg-purple-500' onClick={handleCancel}>Cancel</button>
