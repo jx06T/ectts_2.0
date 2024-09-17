c734048605bc1e32d5608d82ddfdb08664860002
jx06T <50313tjx06@gmail.com>
2024-09-17 09:36:10 +0800

    卡片

--- src/components/CardArea.tsx
+++ src/components/CardArea.tsx
@@ -15,7 +15,7 @@ function Card() {
 function CardArea({ randomTable, words }: { randomTable: number[], words: Word[] }) {
     return (
         // <div className="card-area left-0 right-0 top-0 absolute flex h-full flex-col items-center z-30 bg-blue-200 opacity-80">
-        <div className="card-area left-0 right-0 top-[66px] bottom-20 absolute flex flex-col items-center z-30 bg-transparent">
+        <div className="card-area left-0 right-0 top-[66px] bottom-[70px] absolute flex flex-col items-center z-30 bg-transparent">
             <Card />
         </div>
     )
